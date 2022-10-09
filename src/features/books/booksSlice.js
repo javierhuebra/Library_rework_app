005c3dfc4ded3fdcc67f2bdeb4750c451ba64ee3
javierhuebra <javieremanuelhuebra@gmail.com>
2022-10-09 18:33:09 -0300

    add 2 images for books

--- src/features/books/booksSlice.js
+++ src/features/books/booksSlice.js
@@ -6,7 +6,7 @@ const initialState = [
     title: "100 años de soledad",
     author: "Gabriel García Márquez",
     yearOfPublication: "ni idea",
-    srcImage: "imagen/jeje/aca/hola.jpg",
+    srcImage: "https://covers.alibrate.com/b/59872e8acba2bce50c1a6d96/b0bf30dd-8585-4a46-9229-c72a94282fbe/medium",
     description: "Un libro larguisimo que nunca lei",
     isbn: "no se si esta"
 },
@@ -15,7 +15,7 @@ const initialState = [
     title: "Relatos de un náufrago",
     author: "Gabriel García Márquez",
     yearOfPublication: "ni idea",
-    srcImage: "imagen/jeje/aca/naufragio.jpg",
+    srcImage: "https://contentv2.tap-commerce.com/cover/large/9789871138036_1.jpg?id_com=1113",
     description: "Este si lo leí, esta buenardo",
     isbn: "no se si esta"
 }
